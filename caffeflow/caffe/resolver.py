68b1a47ceeeb4ace72e4a99e592bd3f048fb15a5
Santiago Castro <santiago@xmartlabs.com>
2017-07-04 18:38:49 -0300

    Fall back to use own protobuf if PyCaffe import fails due to error

--- caffeflow/caffe/resolver.py
+++ caffeflow/caffe/resolver.py
@@ -4,27 +4,25 @@ SHARED_CAFFE_RESOLVER = None
 
 class CaffeResolver(object):
     def __init__(self):
-        self.import_caffe()
-
-    def import_caffe(self):
         self.caffe = None
+        self.use_caffe = False
         try:
             # Try to import PyCaffe first
             import caffe
             self.caffe = caffe
-        except ImportError:
+            self.caffepb = self.caffe.proto.caffe_pb2
+            self.use_caffe = True
+        except Exception as e:
             # Fall back to the protobuf implementation
             from . import caffe_pb2
             self.caffepb = caffe_pb2
-            show_fallback_warning()
-        if self.caffe:
-            # Use the protobuf code from the imported distribution.
-            # This way, Caffe variants with custom layers will work.
-            self.caffepb = self.caffe.proto.caffe_pb2
+            sys.stderr.write("INFO: There was an error when trying to import PyCaffe: {}\n"
+                             "Falling back to the bundled protobuf.\n\n".format(e))
         self.NetParameter = self.caffepb.NetParameter
 
     def has_pycaffe(self):
-        return self.caffe is not None
+        return self.use_caffe
+
 
 def get_caffe_resolver():
     global SHARED_CAFFE_RESOLVER
@@ -32,8 +30,6 @@ def get_caffe_resolver():
         SHARED_CAFFE_RESOLVER = CaffeResolver()
     return SHARED_CAFFE_RESOLVER
 
+
 def has_pycaffe():
     return get_caffe_resolver().has_pycaffe()
-
-def show_fallback_warning():
-    sys.stderr.write("INFO: PyCaffe not found! Falling back to the bundled protobuf.\n")
